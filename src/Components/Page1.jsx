8aab8eaec2c2d8649c259834319a17e7451dc8bf
Adarshaadi7572 <114873985+Adarshaadi7572@users.noreply.github.com>
2024-04-20 10:38:44 +0000

    shift utility to the public

--- src/Components/Page1.jsx
+++ src/Components/Page1.jsx
@@ -41,7 +41,7 @@ let Page1 = () => {
               }} />
               <div className={`flex absolute z-10 gap-3 bg-white border border-1 w-96 h-12 items-center pl-2 rounded-md hover: hover:outline-orange-200 hover:outline hover:outline-offset-0 hover:outline-2 hover:shadow-[0_4px_16px_0px_rgba(0,0,0,0.3)] max-[420px]:w-80 max-[333px]:w-64 ${Id == 1 && `outline-orange-200 outline-offset-0 outline-2 outline shadow-[0_4px_16px_0px__rgba(0,0,0,0.3)]`}`} onClick={() => clickeHandler(1)}>
                 <div>
-                  <img src='src/Utilities/icons/student.png' />
+                  <img src='/Utilities/icons/student.png' />
                 </div>
 
                 <label htmlFor="1" className='max-[333px]:text-sm'><span className='font-semibold max-[333px]:text-sm'>Student</span> or soon to be enrolled</label>
@@ -55,7 +55,7 @@ let Page1 = () => {
                           }}/>
               <div className={`flex absolute z-10 gap-3 bg-white border border-1 w-96 h-12 items-center pl-2 rounded-md hover: hover:outline-orange-200 hover:outline hover:outline-offset-0 hover:outline-2 hover:shadow-[0_4px_16px_0px_rgba(0,0,0,0.3)] max-[420px]:w-80 max-[333px]:w-64 ${Id == 2 && `outline-orange-200 outline-offset-0 outline-2 outline shadow-[0_4px_16px_0px__rgba(0,0,0,0.3)]`}`}  onClick={() => clickeHandler(2)}>
                 <div>
-                  <img src='src/Utilities/icons/businessman.png' />
+                  <img src='/Utilities/icons/businessman.png' />
                 </div>
                 <label htmlFor="2" className='max-[333px]:text-sm'><span className='font-semibold max-[333px]:text-sm'>Proffesional</span> persuing a career</label>
               </div>
@@ -67,7 +67,7 @@ let Page1 = () => {
                           }}/>
               <div className={`flex absolute z-10 gap-3 bg-white border border-1 w-96 h-12 items-center pl-2 rounded-md hover: hover:outline-orange-200 hover:outline hover:outline-offset-0 hover:outline-2 hover:shadow-[0_4px_16px_0px_rgba(0,0,0,0.3)] max-[420px]:w-80 max-[333px]:w-64 ${Id == 3 && `outline-orange-200 outline-offset-0 outline-2 outline shadow-[0_4px_16px_0px__rgba(0,0,0,0.3)]`}`}  onClick={() => clickeHandler(3)}>
                 <div>
-                  <img src='src/Utilities/icons/parent.png' />
+                  <img src='/Utilities/icons/parent.png' />
                 </div>
                 <label htmlFor="3" className='max-[333px]:text-sm'><span className='font-semibold max-[333px]:text-sm'>Parent</span> of a School-age Child</label>
               </div>
@@ -79,7 +79,7 @@ let Page1 = () => {
                           }}/>
               <div className={`flex absolute z-10 gap-3 bg-white border border-1 w-96 h-12 items-center pl-2 rounded-md hover: hover:outline-orange-200 hover:outline hover:outline-offset-0 hover:outline-2 hover:shadow-[0_4px_16px_0px_rgba(0,0,0,0.3)] max-[420px]:w-80 max-[333px]:w-64 ${Id == 4 && `outline-orange-200 outline-offset-0 outline-2 outline shadow-[0_4px_16px_0px__rgba(0,0,0,0.3)]`}`}  onClick={() => clickeHandler(4)}>
                 <div>
-                  <img src='src/Utilities/icons/man-avatar.png' />
+                  <img src='/Utilities/icons/man-avatar.png' />
                 </div>
                 <label htmlFor="4" className='max-[333px]:text-sm'><span className='font-semibold max-[333px]:text-sm'>Lifelong</span> learner</label>
               </div>
@@ -91,7 +91,7 @@ let Page1 = () => {
                           }}/>
               <div className={`flex absolute z-10 gap-3 bg-white border border-1 w-96 h-12 items-center pl-2 rounded-md hover: hover:outline-orange-200 hover:outline hover:outline-offset-0 hover:outline-2 hover:shadow-[0_4px_16px_0px_rgba(0,0,0,0.3)] max-[420px]:w-80 max-[333px]:w-64 ${Id == 5 && `outline-orange-200 outline-offset-0 outline-2 outline shadow-[0_4px_16px_0px__rgba(0,0,0,0.3)]`}`}  onClick={() => clickeHandler(5)}>
                 <div>
-                  <img src='src/Utilities/icons/teacher.png' />
+                  <img src='/Utilities/icons/teacher.png' />
                 </div>
                 <label htmlFor="5" className='max-[333px]:text-sm'><span className='font-semibold max-[333px]:text-sm'>Teacher</span></label>
               </div>
@@ -102,7 +102,7 @@ let Page1 = () => {
                   setFormData({[event.target.name] : event.target.value});
                           }}/>
               <div className={`flex absolute z-10 gap-3 bg-white border border-1 w-96 h-12 items-center pl-2 rounded-md hover: hover:outline-orange-200 hover:outline hover:outline-offset-0 hover:outline-2 hover:shadow-[0_4px_16px_0px_rgba(0,0,0,0.3)] max-[420px]:w-80 max-[333px]:w-64 ${Id == 6 && `outline-orange-200 outline-offset-0 outline-2 outline shadow-[0_4px_16px_0px__rgba(0,0,0,0.3)]`}`}  onClick={() => clickeHandler(6)} >
-                <img src='src/Utilities/icons/other.png' />
+                <img src='/Utilities/icons/other.png' />
                 <label htmlFor="6" className='max-[333px]:text-sm'><span className='font-semibold max-[333px]:text-sm'>Others</span></label>
               </div>
             </div>
